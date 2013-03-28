8611e95c1a9b165e585272b7f1b7de33e39b8d32
musik <860577@gmail.com>
2013-03-28 21:25:39 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zhengquan::Application.config.secret_token = 'b21629760bec37a09cdd70081aa57e9d48da52fb4f2f2272766362f26d3ef8a1249b03c811cee6838faa2a5ba0eebfe6eb4d2f9c22e257a81cf50c58ef473b47'
