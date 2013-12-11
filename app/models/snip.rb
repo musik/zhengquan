28ede67d9ac1727f0aeef423fa5ce3fadfa56ac1
musik <cgicn@qq.com>
2013-12-11 13:08:36 +0800

    snips fix

--- app/models/snip.rb
+++ app/models/snip.rb
@@ -1,6 +1,6 @@
 class Snip < ActiveRecord::Base
   attr_accessible :body, :name
   def self.output name
-    RedCloth.new(find_by_name(name).body).to_html rescue nil
+    find_by_name(name).body rescue ""
   end
 end
