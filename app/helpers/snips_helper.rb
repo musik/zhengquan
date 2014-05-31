7a4b0d845ad25d9239a8a3c929730fb21fa761c5
musik <cgicn@qq.com>
2014-05-31 19:53:48 +0800

    home links

--- app/helpers/snips_helper.rb
+++ app/helpers/snips_helper.rb
@@ -1,5 +1,5 @@
 module SnipsHelper
   def snip_get name
-    RedCloth.new(Snip.output(name)).to_html.gsub("<a ","<a target=\"_blank\" ").html_safe rescue ""
+    RedCloth.new(Snip.output(name)).to_html.gsub("<a ","<a target=\"_blank\" ").gsub("<br />",'').html_safe rescue ""
   end
 end
